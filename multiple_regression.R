6e061421ad5b203bff9b7dfddb8be3daa2ff230c
graffv <67825888+graffv@users.noreply.github.com>
2023-01-26 14:44:10 +0100

    Add files via upload

--- multiple_regression.R
+++ multiple_regression.R
@@ -10,40 +10,33 @@ data_periodNot3 = subset(data, period!=3)
 data_period4 = subset(data, period==4)
 
 make_regression <- function(trade, df){
-  regressand <- c("dy", "di", "de", "du", "ty", "ti", "te", "tu", "hy", "hi", "he", "hu", "sy", "si", "se", "su")
+  regressand <- c("dy", "di", "de", "du", "ty", "ti", "te", "tu", "hy", "hi", "he", "hu", "sy", "si", "se", "su", "oy", "oi", "oe", "ou")
   # We store the values of the coefficients of the lr in the associated lists below
   hash_regressand1 <- c()
   hash_regressand2 <- c()
-  hash_regressand_iv1_distance <- c()
-  hash_regressand_iv2_distance <- c()
-  hash_regressand_iv1_adjacent <- c()
-  hash_regressand_iv2_adjacent <- c()
-  hash_regressand_iv1_ling <- c()
-  hash_regressand_iv2_ling <- c()
+  hash_std_lm <- c()
+
+  hash_regressand_iv1 <- c()
+  hash_regressand_iv2 <- c()
 
   for (regres in regressand){
     # For each regressand, linear regression on the trade data selected (wt, lwt, lwm, lwx)
-    model_regres <- rlm(formula <- df[regres][[1]] ~ df[trade][[1]])
+    model_regres <- lm(formula <- df[regres][[1]] ~ df[trade][[1]])
     # Same for the IV
-    model_iv_distance <- ivreg(df[regres][[1]] ~ df[trade][[1]] | distance, data = data)
-    model_iv_adjacent <- ivreg(df[regres][[1]] ~ df[trade][[1]] | adjacent, data = data)
-    model_iv_ling <- ivreg(df[regres][[1]] ~ df[trade][[1]] | ling, data = data)
+    model_iv_complete <- ivreg(df[regres][[1]] ~ df[trade][[1]] | distance + adjacent + ling, data=data)
+
     
     # Store all values in lists
     hash_regressand1[regres] <- c(model_regres$coefficients[1])
     hash_regressand2[regres] <- c(model_regres$coefficients[2])
-    hash_regressand_iv1_distance[regres] <- c(model_iv_distance$coefficients[1])
-    hash_regressand_iv2_distance[regres] <- c(model_iv_distance$coefficients[2])
-    hash_regressand_iv1_adjacent[regres] <- c(model_iv_adjacent$coefficients[1])
-    hash_regressand_iv2_adjacent[regres] <- c(model_iv_adjacent$coefficients[2])
-    hash_regressand_iv1_ling[regres] <- c(model_iv_ling$coefficients[1])
-    hash_regressand_iv2_ling[regres] <- c(model_iv_ling$coefficients[2])
+    
+    hash_regressand_iv1[regres] <- c(model_iv_complete$coefficients[1])
+    hash_regressand_iv2[regres] <- c(model_iv_complete$coefficients[2])
   }
   # Creating matrix of lists to gather all coefficients
-  combi = matrix(c(hash_regressand1, hash_regressand2, hash_regressand_iv1_distance, hash_regressand_iv2_distance,
-                   hash_regressand_iv1_adjacent, hash_regressand_iv2_adjacent,
-                   hash_regressand_iv1_ling, hash_regressand_iv2_ling), ncol=8, nrow=16)
+
+  combi <- matrix(c(hash_regressand1, hash_regressand2, hash_regressand_iv1, hash_regressand_iv2), ncol=4, nrow=20)
   return(combi)
 }
 
-make_regression("wt", data)
+make_regression("lwt", data)
