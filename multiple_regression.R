073eb0be1ca6958a45807c08f27cdae267571be0
graffv <67825888+graffv@users.noreply.github.com>
2023-02-05 19:37:14 +0100

    Add files via upload
    Updated regressions with std errors of estimator: classic, robust, clustered

--- multiple_regression.R
+++ multiple_regression.R
@@ -1,7 +1,8 @@
 library(MASS)
 library(haven)
 library(ivreg)
-#install.packages("ivreg", dependencies = TRUE)
+library(sandwich)
+library(lmtest)
 
 data = read.csv("/Users/victorgraff/Documents/2022:2023 ENSAE/Cours/Statapps/data_StatApp/data_FR_enhanced_dummies.csv")
 data_period1 = subset(data, period==1)
@@ -15,6 +16,8 @@ make_regression <- function(trade, df){
   hash_regressand1 <- c()
   hash_regressand2 <- c()
   hash_std_lm <- c()
+  hash_std_lm_robust <- c()
+  hash_std_lm_robust_cluster <- c()
 
   hash_regressand_iv1 <- c()
   hash_regressand_iv2 <- c()
@@ -22,20 +25,31 @@ make_regression <- function(trade, df){
   for (regres in regressand){
     # For each regressand, linear regression on the trade data selected (wt, lwt, lwm, lwx)
     model_regres <- lm(formula <- df[regres][[1]] ~ df[trade][[1]])
+    model1_robust <- coeftest(model_regres, vcov = vcovHC)
+    model1_robust_clustered <- coeftest(model_regres, vcov = vcovCL, type = "HC1", cluster = df[regres][[1]])
+    
     # Same for the IV
     model_iv_complete <- ivreg(df[regres][[1]] ~ df[trade][[1]] | distance + adjacent + ling, data=data)
 
-    
     # Store all values in lists
     hash_regressand1[regres] <- c(model_regres$coefficients[1])
     hash_regressand2[regres] <- c(model_regres$coefficients[2])
     
     hash_regressand_iv1[regres] <- c(model_iv_complete$coefficients[1])
     hash_regressand_iv2[regres] <- c(model_iv_complete$coefficients[2])
+    
+    # store Std
+    hash_std_lm[regres] <- c(summary(model_regres)$coefficients[4])
+    hash_std_lm_robust[regres] <- c(model1_robust[4])
+    hash_std_lm_robust_cluster[regres] <- c(model1_robust_clustered[4])
   }
   # Creating matrix of lists to gather all coefficients
 
-  combi <- matrix(c(hash_regressand1, hash_regressand2, hash_regressand_iv1, hash_regressand_iv2), ncol=4, nrow=20)
+  combi <- matrix(c(hash_regressand1, hash_regressand2,
+                    hash_regressand_iv1, hash_regressand_iv2,
+                    hash_std_lm,
+                    hash_std_lm_robust, hash_std_lm_robust_cluster),
+                  ncol=7, nrow=20)
   return(combi)
 }
 
